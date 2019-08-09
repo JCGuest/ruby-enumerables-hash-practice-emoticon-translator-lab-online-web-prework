b23d1b4f16757e22ab67af3f7ddda3d0c12844b1
John Guest <jcguest90@gmail.com>
2019-08-09 05:37:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,3 @@
-require "yaml"
-
-def load_library(file_path)
-emojis = YAML.load_file(file_path)
-
-new_hash = {
-  'get_meaning' => {},
-  'get_emoji' => {}
-}
-
-emojis.each do |key, value| 
-  new_hash['get_meaning'][value[1]] = key
-  new_hash['get_emoji'][value[0]] = emojis[key][1]
-  end
 
 end
 
